2a7364d1fd8762cb102382af46d5d13d9d93d0f0
Christian Walker <christiandwalker2021@gmail.com>
2025-04-02 15:44:21 -0500

    adds utils

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,7 @@ export const auth = betterAuth({
   databaseHooks: {
     user: {
       create: {
+        // used in order to break up the first and last name into separate fields
         before: async (user) => {
           // split the name into first and last name (name object is mapped to the first name by the config)
           const [firstName, ...rest] = user.name.split(" ");
@@ -26,12 +27,15 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: false,
   },
+  // cookie cache is enabled to speed up the session retrieval to avoid calling the database each time
+  // https://better-auth.vercel.app/docs/concepts/session-management#cookie-cache
   session: {
     cookieCache: {
       enabled: true,
       maxAge: 5 * 60,
     },
   },
+  // social login is enabled for google
   socialProviders: {
     google: {
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
@@ -39,9 +43,11 @@ export const auth = betterAuth({
     },
   },
   user: {
+    // this maps the default "name" field to the "firstName" field in the database
     fields: {
       name: "firstName",
     },
+    // this declares the extra fields that are not in the default user schema that better auth creates, but are in the database
     additionalFields: {
       firstName: {
         type: "string",
@@ -63,6 +69,9 @@ export const auth = betterAuth({
   advanced: {
     cookiePrefix: "O",
   },
+  // this disables the default signup and signin routes, as we are using our own custom routes
   disabledPaths: ["/api/auth/signup", "/api/auth/signin/email"],
+  // this allows for server actions to use cookies
+  // https://better-auth.vercel.app/docs/integrations/next#server-action-cookies
   plugins: [nextCookies()],
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { betterFetch } from "@better-fetch/fetch";
+import type {Session} from "./types"
+import { NextRequest } from "next/server";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+
+export async function getServerAuth(baseURL:string, cookie:string) {
+  return betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL,
+      headers: {
+        cookie
+      },
+    }
+  );
 }
