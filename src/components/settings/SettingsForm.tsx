097a24d0a8e1ac0411309a624aee5d1a0c0353e9
Dynadinomo <sifuentesgael2003@gmai.com>
2025-05-09 12:38:36 -0500

    Doing the thing

--- src/components/settings/SettingsForm.tsx
+++ src/components/settings/SettingsForm.tsx
@@ -5,7 +5,10 @@ import z from 'zod';
 import { updateSettingsActions } from '@/actions/settings';
 import { useAction } from 'next-safe-action/hooks';
 import { Loader2 } from 'lucide-react';
-
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { Button } from '../ui/button';
+import { Toaster } from 'sonner';
 
 type UserProps = z.infer<typeof UserSettingsSchema>;
 
@@ -21,6 +24,10 @@ export default function SettingsForm(userSettings: UserProps) {
     const { execute, status } = useAction(updateSettingsActions, {
         onSuccess: ({ }) => {
             console.log("Updated Succesfuly")
+        },
+        onError: ({ }) => {
+            <Toaster></Toaster>
+
         }
     })
     const isLoading = status === "executing"
@@ -36,11 +43,24 @@ export default function SettingsForm(userSettings: UserProps) {
                 execute(data)
 
             })}>
-                <input {...register("firstName", { required: true })} placeholder='First name' />
+                {/* <input {...register("firstName", { required: true })} placeholder='First name' />
                 <input {...register("lastName", { required: true, minLength: 4 })} placeholder='Last Name' />
-                <input {...register("pronouns", { required: true })} placeholder='They/Them' />
+                <input {...register("pronouns", { required: true })} placeholder='They/Them' /> */
 
-                {isLoading ? (<Loader2 className="animate-spin" />) : (<input type="submit" />)}
+                    <div className='items-center justify-center'>
+                        <Label>First Name</Label>
+                        <Input {...register("firstName")} placeholder="First Name" className='w-50'></Input>
+
+                        <Label>Last Name</Label>
+                        <Input {...register("lastName")} placeholder="Last Name" className='w-100'></Input>
+
+                        <Label>Pronouns</Label>
+                        <Input {...register("pronouns")} placeholder="They/Them" className='w-50'></Input>
+                    </div>
+
+                }
+
+                {isLoading ? (<Loader2 className="animate-spin" />) : (<Button type="submit" className='border'>Submit</Button>)}
 
             </form>
         </div>
